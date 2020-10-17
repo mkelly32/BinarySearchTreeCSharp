81bdb5a5ecaaaa0938ca59b8a392971a127ebc2e
Mike Kelly <mkelly32@tcd.ie>
2020-10-17 23:08:25 +0100

    Initial Commit

---
+++ LCA/BinaryTree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LCA
+{
+    public class BinaryTree
+    {
+        public class Node
+        {
+            private int data;
+            private Node lesserTree;
+            private Node greaterTree;
+            public void DisplayNode()
+            {
+                Console.Write(data);
+            }
+        }
+        public Node root;
+    }
+}
